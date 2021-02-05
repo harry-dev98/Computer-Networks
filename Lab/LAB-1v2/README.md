9d1b2ef33256aaca8ff6ee88dbbca81b960c8a29
harry-dev98 <harjeet.769644@gmail.com>
2021-02-05 09:56:37 +0530

    Server with req. calls in LAB-1

---
+++ Lab/LAB-1v2/README.md
@@ -0,0 +1,18 @@
+### IMP System calls Used:
+` poll() `
+` getpgid() `
+` killpg() `
+` popen2_ForFdAndPid() `
+` dup2 `
+` execv `
+` pipe() `
+` mkfifo() `
+` signal() `
+
+
+### signals used
+` SIGINT `
+` SIGCONT `
+` SIGSTOP `
+` SIGUSR1 `
+` SIGUSR2 `
--- Lab/LAB-1v2/server.cpp
+++ Lab/LAB-1v2/server.cpp
@@ -8,6 +8,7 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <fcntl.h>
+#include <unistd.h>
 
 using namespace std;
 #define SCH 1
@@ -23,44 +24,73 @@ int sigCount = 0;
 char buffer[1024];
 int idx_end = 0;
 int idx_start = 0;
+int popen_fd, pipe_fd;
 
-// void exit(int signum){
-//     cout<<"Total signals recieved: "<<sigCount<<"\n";
-//     signal(SIGINT, SIG_DFL);
-//     raise(SIGINT);
-//     exit(0);
-// }
+#define READ 0
+#define WRITE 1
+
+int POPENPGID=1000001, PIPEPGID = 1000002;
+
+
+// assumed new process will just read so type always == "w"
+// custom popen2 as per the requirement of situation
+int popen2_ForFdAndPid(string cmd, int& newpid, int newPGId){
+    int fd[2];
+    pipe(fd);
+    if((newpid = fork())==-1){
+        perror("fork");
+    }
+    if(newpid == 0){
+        close(fd[WRITE]);
+        dup2(fd[READ], 0);
+
+        // added new process A to POPENPGID
+        setpgid(0, newPGId);
+        char* const args[]= {NULL, }; 
+        execvp(cmd.c_str(), args);
+    }
+    close(fd[READ]);
+    return fd[WRITE];
+}
+
+void sigintHandler(int signum){
+    cout<<"Total signals recieved: "<<sigCount<<"\n";
+    signal(SIGINT, SIG_DFL);
+    raise(SIGINT);
+    exit(0);
+}
 
 void* sendDataViaPopen(void* args){
     cout<<"sending data to via POPEN\n";
-    FILE* writer = popen(PROCESSA, "w");
-    int _fd = fileno(writer);
-    perror("popen");
     while(idx_start <= idx_end){
         sleep(1);
-        write(_fd, &buffer[idx_start++], sizeof(char));
+
+        // used custom popen to do setpgid on new process
+        write(popen_fd, &buffer[idx_start++], sizeof(char));
         cout<<idx_start<<"send popen\n";
     }
     cout<<"no more data to send\n";
-    close(_fd);
     pthread_exit(0);   
 }
 
 void* sendDataViaPipe(void* args){
     cout<<"sending data via PIPE\n";
-    int fd = open(FROMS, O_WRONLY);
     while(idx_start <= idx_end){
         sleep(1);
-        write(fd, &buffer[idx_start++], sizeof(char));
+
+        // used named pipe 
+        write(pipe_fd, &buffer[idx_start++], sizeof(char));
         cout<<idx_start<<"send pipe\n";
     }
     cout<<"no more data to send\n";
-    close(fd);
     pthread_exit(0);   
 }
 
 void* recvData(void* args){
     cout<<"recieving data from CD\n";
+
+
+    // used named pipe
     int fd = open(FROMC, O_RDONLY);
     perror("recvData");
     while(1){
@@ -78,6 +108,15 @@ void handleSIGUSR1(int signum){
     if(sigCount!=0){
         pthread_cancel(th);
     }
+
+
+    // used killpg
+    // used SIGSTOP
+    // used SIGCONT
+
+    killpg(POPENPGID, SIGSTOP);
+    killpg(PIPEPGID, SIGCONT);
+    
     pthread_create(&th, NULL, &sendDataViaPipe, NULL);
     sigCount++;
 }
@@ -87,36 +126,61 @@ void handleSIGUSR2(int signum){
     if(sigCount!=0){
         pthread_cancel(th);
     }
+
+    // used killpg
+    // used SIGSTOP
+    // used SIGCONT
+
+    killpg(PIPEPGID, SIGSTOP);
+    killpg(POPENPGID, SIGCONT);
     pthread_create(&th, NULL, &sendDataViaPopen, NULL);
     sigCount++;
 }
 
 int main(){
-    // signal(SIGINT, exit);
+    signal(SIGINT, sigintHandler);
     mkfifo(FROMS, 0666);
     perror("froms");
     mkfifo(FROMC, 0666);
     perror("fromc");
     mkfifo(SIG, 0666);
     perror("sig");
+
     int _fd = open(SIG, O_WRONLY);
     int _pid = getpid();
     write(_fd, &_pid, sizeof(int));
     close(_fd);
-
+    
     cout<<"D knows my pid now!!\n";
     int pid;
     for(int i=0; i<SCH; i++){
         pid = fork();
         if(pid == 0)break;
+
+        // adding all the childs to PIPEPGID group
+        setpgid(0, PIPEPGID);
+
     }
 
-    if(pid != 0){
+    if(pid > 0){
         signal(SIGUSR1, handleSIGUSR1);
         signal(SIGUSR2, handleSIGUSR2);
+        int readerPid;
+        popen_fd = popen2_ForFdAndPid(PROCESSA, readerPid, POPENPGID);
+        pipe_fd = open(FROMS, O_WRONLY);
+
+
+        // used killpg
+            // used SIGSTOP
+            // used SIGCONT
+        killpg(POPENPGID, SIGSTOP);
+        killpg(PIPEPGID, SIGSTOP);
+
         pthread_create(&th, NULL, &recvData, NULL);
 
     } else if(pid == 0){
+
+        //used poll
         pollfd fd[0];
         fd[0].fd = open(FROMS, O_RDONLY);
         fd[0].events = POLLIN;
@@ -137,6 +201,9 @@ int main(){
         cout<<"waiting for child "<<i<<".\n";
         wait(NULL);
     }
+    pthread_join(th, NULL);
+    close(popen_fd);
+    close(pipe_fd);
     cout<<"Total signals recieved: "<<sigCount<<"\n";
     return 0;
 }
